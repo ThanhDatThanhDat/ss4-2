df5ca7f247d61f36806b9684adc1452ddf305512
ThanhDatThanhDat <dt2229992@gmail.com>
2024-11-22 17:42:25 +0700

    ss4-2

---
+++ ss4-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Vui long nhap vao mot so nguyen: ");
+    scanf("%d", &num);
+
+    if (num%2==0){
+        printf("So %d la so chan.\n", num);
+    } else {
+        printf("So %d la so le.\n", num);
+    }
+
+    return 0;
+}
